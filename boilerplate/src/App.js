a57f23600367dcf6d54e7f3c5cd9db13f51ceabb
juan <jpalacio@axletravel.com>
2022-01-04 00:07:28 -0600

    Removed redundant info

--- boilerplate/src/App.js
+++ boilerplate/src/App.js
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import TableExample from "./Components/Table";
-import SideSheetExample from "./Components/SideSheet";
 import ItemsBasket from "./Components/ItemsBasket/index";
 
 function App() {
   return (
     <div className="App">
-      <div className="content-chunk">
-        <TableExample/>
-      </div>
-      <div className="content-chunk">
-      <SideSheetExample/>
-      </div>
       <div className="content-chunk">
         <ItemsBasket/>
       </div>
