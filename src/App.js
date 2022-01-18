fedd7798400f13201151a396c1f0a07fc7095456
juan <jpalacio@axletravel.com>
2022-01-18 16:02:01 -0500

    Committing before bringing in simple backend using Express

--- src/App.js
+++ src/App.js
@@ -29,6 +29,25 @@ function App() {
     setCollapsed(!collapsed)
   };
 
+  const IS_DEV_ENVIRONMENT = true;
+  const BASE_URL = IS_DEV_ENVIRONMENT ? "localhost:3001/" : "www.backend-server-url.com";
+
+ 
+  async function postData(url = BASE_URL , data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
 
   return (
     <div className="App">
