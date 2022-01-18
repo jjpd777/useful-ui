a9c89c812a1121b778cbe70fa57b712605ad58f5
juan <jpalacio@axletravel.com>
2022-01-18 16:39:02 -0500

    Added basic async function call to the backend using a Button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import InvoicesTable from "./Components/InvoicesTable/index";
 import DeliverInvoice from "./Components/DeliverInvoice/index";
 import StripeResponse from "./Components/StripeResponse/index";
 import Home from "./Components/Home/index";
+import { Button } from 'antd';
+
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import 'antd/dist/antd.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
@@ -24,33 +26,35 @@ const { SubMenu } = Menu;
 
 function App() {
 
-  const [collapsed, setCollapsed] = useState(false);
-  const onCollapse = () =>{
-    setCollapsed(!collapsed)
-  };
-
   const IS_DEV_ENVIRONMENT = true;
-  const BASE_URL = IS_DEV_ENVIRONMENT ? "localhost:3001/" : "www.backend-server-url.com";
+  const BASE_URL = IS_DEV_ENVIRONMENT ? "http://www.localhost:3001/" : "www.backend-server-url.com";
 
  
-  async function postData(url = BASE_URL , data = {}) {
-    // Default options are marked with *
+  async function postData(url = BASE_URL , data = {customerID:"ABC1234"}) {
+
     const response = await fetch(url, {
       method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: 'same-origin', // include, *same-origin, omit
+      mode: 'cors', 
+      credentials: 'same-origin',  
       headers: {
         'Content-Type': 'application/json'
-        // 'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: JSON.stringify(data) // body data type must match "Content-Type" header
+      body: JSON.stringify(data)
     });
-    return response.json(); // parses JSON response into native JavaScript objects
+    console.log("This is the backend response: ",response);
+    return response.json(); 
   }
 
   return (
     <div className="App">
+      <div>
+      <br></br>
+      <br></br>
+      <br></br>
+        <Button onClick={()=>{ postData()}}>
+            This Button Makes a Backend Call
+        </Button>
+      </div>
       <Routes>
         <Route exact path="/" element ={<Home/>}>
           <Route path="/invoice" element={<CreateInvoice />} />
